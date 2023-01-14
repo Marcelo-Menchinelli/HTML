d92c27d16c1310074475fbd9f83ca9ddf9d50661
marcelo-menchinelli <menchinelli77@gmail.com>
2023-01-14 11:27:27 -0300

    Adicionado aplicações JS

---
+++ JavaScript/Potencia.js
@@ -0,0 +1,8 @@
+
+// Como da conta "Potencia"
+console.log(Math.pow(2,2))
+console.log(Math.pow(2,4))
+
+// Ou posso fazer assim também que é igual:
+console.log(2 ** 2)
+console.log(2 ** 4)
---
+++ JavaScript/operaçõesMatematicas.js
@@ -0,0 +1,50 @@
+//Soma
+20+20
+
+
+//multiplicação
+20-20
+
+
+//divisão
+20/20
+
+
+//Resto
+console.log(7%3)
+
+
+//incremento
+let myNumber =10
+console.log(myNumber + 1)
+//Ou assim também incrementa:
+let myNumber1 =10
+myNumber1++
+myNumber1++
+myNumber1++
+myNumber1++
+console.log(myNumber1)
+//Ou assim também incrementa:
+let MyNumber10 = 10
+console.log(++myNumber10)
+
+
+//decremento
+let myNumber2 =10
+console.log(myNumber2 + 1)
+//Ou assim também decrementar:
+let myNumber3 =10
+myNumber3--
+myNumber3--
+myNumber3--
+myNumber3--
+console.log(myNumber3)
+//Ou assim também decrementar:
+let myNumber10 = 10
+console.log(--myNumber10)
+
+
+//Soma
+//Soma
+//Soma
+//Soma
